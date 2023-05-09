8433136cb1406eeb1501679ba865405aecbeadc2
yakob-abada <yakob.abada@gmail.com>
2023-05-09 15:57:04 +0200

    Introduce DDD design pattern

---
+++ src/Bootstrap/NewProductController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Bootstrap;
+
+use Controller\ProductController;
+use Repository\ProductRepository;
+
+class NewProductController
+{
+    public function create(): ProductController
+    {
+        $mysqli = new \mysqli("localhost","root","","scandiweb");
+
+        if ($mysqli->connect_errno) {
+            echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+            exit();
+        }
+
+        return new ProductController(new ProductRepository($mysqli));
+    }
+}
