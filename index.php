28e642107e785ae97c138ab3e3b7d75f39e5c2e7
yakob-abada <yakob.abada@gmail.com>
2023-06-06 15:14:38 +0100

    pump up FTP version

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Origin: http://scandiweb-app.s3-website-eu-west-1.amazonaws.com");
 header("Access-Control-Allow-Headers: Content-Type");
-header("Access-Control-Allow-Methods: GET, POST, DELETE");
+header("Access-Control-Allow-Methods: *");
 
 require_once('bootstrap.php');
---
+++ test.php
@@ -0,0 +1,3 @@
+<?php
+echo 'My username is ' .$_ENV["DATABASE_HOST"] . '!';
+?>
